5bf0c270d3fa8cd1553454030d86477e4bdc5367
Shipalan <Shipalan678@gmail.com>
2022-05-27 15:28:24 -0600

    correcting order mistake on sldc.js

--- sldc.js
+++ sldc.js
@@ -8,8 +8,8 @@
 // Planning
 // Analysis of Requirements
 // Design
-// Testing & Integration
 // Implementation
+// Testing & Integration
 // Maintain
 
 
@@ -21,6 +21,6 @@
 let planning = ('Creating a plan to have a clear scope of what needs to be done to achieve the set goals.');
 let analysis = ('Gather all specific details required for a new system.');
 let design = ('Outline the details for the overall application. Operation, training, and maintenance plans will all be drawn up so that developers know what they need to do throughout every stage of the cycle moving forward.');
+let implementation = ('Actual coding portion');
 let testingIntegration = ('Testing the software to make sure there are not any bugs. The software is moved into its environment and eventually installed');
-let implementation = ('Implementing the software into the market for end users');
 let maintain = ('Handle any bugs that come up from user reports.');
